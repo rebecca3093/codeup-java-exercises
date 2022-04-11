e6955541dfbf6ad197ea8bfb1ca405caab9cef35
Rebecca Martinez <rebecca.m.martinez30@gmail.com>
2022-04-11 16:25:41 -0500

    java exercises

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class MethodsExercises {
 
     public static void main(String[] args) {
@@ -9,15 +11,18 @@ public class MethodsExercises {
         addStuff(10, 5);
         subtractStuff(10, 5);
         multiplyStuff(10, 5);
-        multiplyStuffBonus(10,5,0);
-        divideStuff(10, 0);
+        //multiplyStuffBonus(10,5,0);
+        divideStuff(10, 5);
         modStuff(23, 5);
 
         //2/
-        //getInteger();
+        getInteger(1, 10);
 
         //3.
-        //getFactorial();
+        getFactorial(1, 10);
+
+        //4.
+
 
         System.out.println("End");
     }
@@ -29,7 +34,7 @@ public class MethodsExercises {
     public static int addStuff(int x, int y) {
         //System.out.println("Lets add some stuff:" + (x + y));
         int sum = (x + y);
-        //System.out.println(x + " + " + y + " = " + sum);
+        System.out.println(x + " + " + y + " = " + sum);
         return sum;
     }
 
@@ -40,28 +45,32 @@ public class MethodsExercises {
 
     public static void multiplyStuff(int x, int y) {
         // System.out.println("Bonus stuff after");
-        int sum2 = x;
-        int i;
+        //int sum2 = x;
+        //int i;
 
         // Bonus A.
         //How to call a method and return the value using a previous method
-//        for (i = 1; i < y ; i++ ) {
-//            sum2 = addStuff(sum2,x);
-//            System.out.println("*** sum = " + sum2);
-//        }
+    /*    for (i = 1; i < y; i++) {
+            sum2 = addStuff(sum2, x);
+            System.out.println("*** sum = " + sum2);
+        }
 
         //The easy way (according to Bryant)
-        /*for (i = 1; i < y; i++) {
+        for (i = 1; i < y; i++) {
             sum2 = (sum2 + x);
             System.out.println("sum = " + sum2);
         }*/
+
         //OG CODE!
-        //int sum = (x * y);
-        //System.out.println(/*x + " * " + y + " = " + sum*/ "sum = " + sum);
+        int sum = (x * y);
+        System.out.println(x + " * " + y + " = " + sum);
     }
-// Bryant did this. Im not that smart
+
+// Bryant did this. Im not that skilled. This is an example.
    /* public static int multiplyStuffBonus(int x, int y, int sum3) {
+
         // Bonus B.
+
         if(sum3 == (x*y))
         {
             System.out.println("sum = " + sum3);
@@ -76,6 +85,15 @@ public class MethodsExercises {
         }
         return sum3;
     }*/
+
+
+    // Ry's Example
+
+  /*  if (x == 0 || y == 0){
+        return 0;
+    }
+    return ( x + multiplyByAdd (x, y -1));
+    */
 
     public static void divideStuff(int x, int y) {
 
@@ -98,36 +116,42 @@ public class MethodsExercises {
 
     // 2.
 
- /*   public static int getInteger(int min, int max) {
+    public static int getInteger(int min, int max) {
+
         Scanner scanner = new Scanner(System.in);
         System.out.printf("Enter a number between 1 and 10", min, max);
         int userInput = scanner.nextInt();
         if (userInput < max && userInput > min) {
-            System.out.printf("Your number is between 1 and 10", min, max);
+            System.out.printf("Your number is between 1 and 10 ", min, max);
         } else if (userInput > max || userInput < min) {
             getInteger(min, max);
         }
         return userInput;
-    }*/
+    }
 
     // 3.
+  /*  public static void getFactorial(int min, int max) {
 
-   /* public static void getFactorial(userInput) {
-
-        int num = 10;
-        long factorial = 1;
-
-
-        for (int i = 1; i < num; ++i) {
-            factorial = i;
-        }
-        System.out.printf("Factorial of %d = %d", num, factorial);
-
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("Enter a number between 1 and 10", min, max);
+        int userInput = scanner.nextInt();
     }*/
 
+    //rys walkthru
+    public static void getFactorial(int min, int max) {
 
-}
+        int userInt = getInteger(min, max);
+
+        long result = 1;
+
+        for (int i = 1; i <= userInt; i++) {
+            result += i;
+            fact *= i;
+        }
+        return result;
+
+
+    }
 
 
 
